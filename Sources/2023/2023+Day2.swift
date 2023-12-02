ea5dda7f2166e266de6031b7f5ae7450a3b0eda6
Frank Rupprecht <frank@digitalmasterpieces.com>
2023-12-02 10:24:56 +0100

    Day 2.

---
+++ Sources/2023/2023+Day2.swift
@@ -0,0 +1,96 @@
+import Algorithms
+import RegexBuilder
+
+
+extension Challenges2023 {
+
+    struct Cubes {
+        enum Color { case red, green, blue }
+
+        let color: Color
+        let count: Int
+
+        init(string: String) {
+            let regex = Regex {
+                TryCapture {
+                    OneOrMore(.digit)
+                } transform: {
+                    Int($0)
+                }
+                One(.whitespace)
+                TryCapture {
+                    ChoiceOf {
+                        "red"
+                        "green"
+                        "blue"
+                    }
+                } transform: {
+                    switch $0 {
+                        case "red": Color.red
+                        case "green": Color.green
+                        case "blue": Color.blue
+                        default: fatalError()
+                    }
+                }
+            }
+            let match = string.wholeMatch(of: regex)!.output
+            self.count = match.1
+            self.color = match.2
+        }
+    }
+
+    struct Pull {
+        let cubes: [Cubes]
+
+        init(string: String) {
+            self.cubes = string.split(separator: /,\s/).map { Cubes(string: String($0)) }
+        }
+
+        static func parse(line: String) -> [Pull] {
+            line.split(separator: /;\s/).map { Pull(string: String($0)) }
+        }
+    }
+
+    struct Game {
+        let id: Int
+        let pulls: [Pull]
+
+        init(line: String) {
+            //Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+            let regex = /Game (?<id>\d+): (?<pulls>.*)/
+            let match = line.wholeMatch(of: regex)!
+            self.id = Int(match.output.id)!
+            self.pulls = Pull.parse(line: String(match.output.pulls))
+        }
+
+        func isPossible(maxReds: Int, maxGreens: Int, maxBlues: Int) -> Bool {
+            let numReds = self.pulls.flatMap(\.cubes).filter({ $0.color == .red }).map(\.count).max()!
+            let numGreens = self.pulls.flatMap(\.cubes).filter({ $0.color == .green }).map(\.count).max()!
+            let numBlues = self.pulls.flatMap(\.cubes).filter({ $0.color == .blue }).map(\.count).max()!
+            return numReds <= maxReds && numGreens <= maxGreens && numBlues <= maxBlues
+        }
+
+        var power: Int {
+            let maxReds = self.pulls.flatMap(\.cubes).filter({ $0.color == .red }).map(\.count).max()!
+            let maxGreens = self.pulls.flatMap(\.cubes).filter({ $0.color == .green }).map(\.count).max()!
+            let maxBlues = self.pulls.flatMap(\.cubes).filter({ $0.color == .blue }).map(\.count).max()!
+            return maxReds * maxGreens * maxBlues
+        }
+    }
+
+    
+
+    @discardableResult static func runDay2(input: Input) async throws -> ChallengeResult {
+        let games = input.lines().map(Game.init)
+
+        let part1 = games.filter({ $0.isPossible(maxReds: 12, maxGreens: 13, maxBlues: 14) }).map(\.id).reduce(0, +)
+        print("Part 1: The sum of IDs of possible games is \(part1)")
+
+        let part2 = games.map(\.power).reduce(0, +)
+        print("Part 2: The sum all games' Power is \(part2)")
+
+        return ("\(part1)", "\(part2)")
+    }
+
+
+}
--- Tests/AdventOfCode2023Tests.swift
+++ Tests/AdventOfCode2023Tests.swift
@@ -35,4 +35,17 @@ final class AdventOfCode2023Tests: XCTestCase {
         XCTAssertEqual(result3.part2, "200")
     }
 
+    func testDay2() async throws {
+        let input1 = TestInput(string: """
+            Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+            Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+            Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+            Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+            Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+            """)
+        let result1 = try await Challenges2023.runDay2(input: input1)
+        XCTAssertEqual(result1.part1, "8")
+        XCTAssertEqual(result1.part2, "2286")
+    }
+
 }
